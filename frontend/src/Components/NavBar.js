23c07d95a68d6a2163f6d87d3c7d91b5bcb4c56a
priteshkulshreshtha <66051955+priteshkulshreshtha@users.noreply.github.com>
2022-07-04 19:12:35 +0530

    Update NavBar.js

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,38 +1,50 @@
-import {AppBar, styled, Toolbar,Button, Grid, Typography} from "@mui/material"
-import React from 'react'
+import {
+  AppBar,
+  styled,
+  Toolbar,
+  Button,
+  Grid,
+  Typography,
+} from '@mui/material';
+import React from 'react';
 
-const StyledToolbar=styled(Toolbar)({
-    display:"flex",
-    justifyContent:"space-between",
-    color:"#2EC4B6"
-  })
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+  color: '#2EC4B6',
+});
 
-const CustomBtn=styled(Button)({
-    display: 'flex',
-    color: "white",
-    margin: "-290px",
-    justifyContent: "center"
+const CustomBtn = styled(Button)({
+  display: 'flex',
+  color: 'white',
+  margin: '-290px',
+  justifyContent: 'center',
+});
 
-
-})
-
-  
 const NavBar = () => {
-   return (
+  return (
     <div>
-        <AppBar position="static" sx={{ bgcolor: "#1B2845" }}>
+      <AppBar position="static" sx={{ bgcolor: '#1B2845' }}>
         <StyledToolbar>
-          <Typography variant="h4" fontWeight="600"> Medic</Typography>
+          <Typography variant="h4" fontWeight="600">
+            {' '}
+            Medic
+          </Typography>
           <CustomBtn variant="text"> Home </CustomBtn>
           <CustomBtn variant="text"> Doctors </CustomBtn>
           <CustomBtn variant="text"> Services </CustomBtn>
           <CustomBtn variant="text"> Reviews </CustomBtn>
-          
-          <Button variant="contained" sx={{backgroundColor: "#fc9105",color: "white", margin:1}}>Contact Us </Button>
+
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: '#fc9105', color: 'white', margin: 1 }}
+          >
+            Contact Us{' '}
+          </Button>
         </StyledToolbar>
-        </AppBar>
+      </AppBar>
     </div>
-   )
- }
- 
- export default NavBar
+  );
+};
+
+export default NavBar;
