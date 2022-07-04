4c5f8651bf0232d53848157a6d8bce61354ccf57
priteshkulshreshtha <66051955+priteshkulshreshtha@users.noreply.github.com>
2022-07-04 19:01:37 +0530

    solving conflicts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './App';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './Config/MuiTheme';
-=======
-import App from './App'; 
->>>>>>> ff2110390efcbcf0c1f8ed78edf14e6074a91dbb
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
